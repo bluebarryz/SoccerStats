d2d5911a459128ed317f61ed616cf9eceae1e937
Barry Zhang <45883553+bluebarryz@users.noreply.github.com>
2023-05-11 18:12:01 -0400

    Updates

---
+++ precache-manifest.69641ca4825041351b33614a7c33011a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bab643a697abeab76a8",
+    "url": "/SoccerStats/static/css/main.b4b378da.chunk.css"
+  },
+  {
+    "revision": "1bab643a697abeab76a8",
+    "url": "/SoccerStats/static/js/main.1bab643a.chunk.js"
+  },
+  {
+    "revision": "6ce0619199471a18ccc7",
+    "url": "/SoccerStats/static/js/1.6ce06191.chunk.js"
+  },
+  {
+    "revision": "5ecede3645673b9b1222",
+    "url": "/SoccerStats/static/js/runtime~main.5ecede36.js"
+  },
+  {
+    "revision": "43b2a1f27bb3d40f29334cf31797931d",
+    "url": "/SoccerStats/index.html"
+  }
+];
